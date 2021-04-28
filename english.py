02f94fca90327e968cf95ddce28695e4063a52a8
Marcus <marcuspanozzo@gmail.com>
2021-04-27 22:02:46 -0400

    Added major components of definitions for classes Sentance Class: Represents a list of words essentially has basic container function
    AnalyzedSentance Class: Represents a fully broken down
    list of words, which are broken down and categorized by part
    of speach. The sentance also has it's type determined

--- english.py
+++ english.py
@@ -1,3 +1,4 @@
+
 '''
 English is a language which evolved from other germanic languages in northwestern europe
 It has rules called grammar which dictate what words do and how they behave or express
@@ -53,17 +54,99 @@ Interjection: Word used to express emotion
 
 '''
 
-class Word(object):
+class Word(string):
     """
     Represents a single english word
     """
-    pass
+    'Inherits string version of __init__
+    self.isPunct = False
+    
+    def isPunct(self):
+        self.isPunct = !isalnum(self)
+        return !isalnum(self) 
+
+class Verb(Word):
+    """
+    Represents a Verb in the English Language
+    """
+
+class Punctuation:
+    period="."
+    question="?"
+    exclaim="!"
+    comma=","
+    semicolon=";"
+    colon=":"
+    
+
+class SentanceTypes:
+    declaration=0
+    imperative=1
+    interrogative=2
+    exclamation=3
 
 class Sentance(object):
     """
     Represents a string of words which are structured
     in a specific way to declare something, dictate a command, ask a question,
-    or exclaim something
+    or exclaim something.
+   
     """
-    pass
+
+    def __init__(self,rawtext = ""):
+        self.rawtext = rawtext
+        self.words = []
+        self.punct = []
+        self.indepClauses = [] 'Can act as their own sentances'
+
+    '''
+    Returns a list of tuples which stores the index and the punctuation mark 
+    '''
+    def __determinePunctuation(self):
+        puncts = []
+        for part in self.parts:
+          pass
+        return puncts
+
+    '''
+    Returns a list of tuples of words and the index in order of occurence. Does not include punctuation
+    '''
+    def __determineWords(self):
+        pass
+
+class AnalyzedSentance(Sentance):
+    """
+    Represents a sentance which upon construction has words broken
+    down by part of speech, as well as determining independent and dependent clauses
+    Uses a dictionary to determine the parts of speech for words and to perform analysis
+    on the sentance structure
+    """
+
+    def __init__(self,rawtext="",dictionary="",grammarbook=""):
+        Sentance.__init__(self,rawtext)
+        self.words = __determineClassifiedWords()
+        self.sentanceType = 
+    '''
+    Makes analysis of sentance to find commas and conjunctions which do not represent 
+    collections gathering in order to find parts of this sentance which themselves
+    could be considered sentances
+    ex. "I am a witch, but are you a witch?"
+        I am a witch. (declaration) are you a witch? (question)
+    '''
+    def __determineIndependentClauses(self):
+        pass
+    '''
+    Returns a list of tuples (word,index of occurence) where each word is classified
+    into parts of speech (word will be one of the childclasses of "Word")
+    '''
+    def __determineClassifiedWords(self):
+        pass
+
+    '''
+    Returns the type of sentance. Will either be DECLARATION
+    '''
+    def __determineSentanceType(self):
+        pass
+
+    
 
