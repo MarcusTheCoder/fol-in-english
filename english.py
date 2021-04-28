1028f89ffb77081c09696564efc32e325905e649
Marcus <marcuspanozzo@gmail.com>
2021-04-27 22:20:52 -0400

    Updated comment formatting and placement

--- english.py
+++ english.py
@@ -1,5 +1,5 @@
 
-'''
+"""
 English is a language which evolved from other germanic languages in northwestern europe
 It has rules called grammar which dictate what words do and how they behave or express
 information in sentances
@@ -18,41 +18,65 @@ A sentance must include some noun or verb being used
     ex. Ouch! -- This is an exclamative (exclamation)
         -- (we probably do not need to implement exclamative sentances,
             so we can say a '!' denotes an imperative)
-'''
-'''
-Punctuation Marks
+"""
+class SentanceTypes:
+    DECLARATION=0
+    IMPERATIVE=1
+    INTERROGATIVE=2
+    EXCLAMATION=3
 
-. period : Indicates a declaration or imperative
-    -- (Since we are not evaluating Interjections, )
-? query  : Denotes a question
-    -- In our case this could mean a goal we are trying to prove
-    -- or the head of a subproof we want to prove
-! exclaim: Denotes an exclamation or (for our program) imperative
-'''
+class Punctuation:
+    """
+    Punctuation Marks
 
-'''
-9 Parts of Speech
-
--- Main words --
-Verb : action or state of being
-Noun: person, place, or thing
-Pronoun: Word used in place of a noun
-
--- modifiers/descriptors--
-Adjective: modifies or describes a noun or pronoun
-Adverb: modifies or describes a verb
-
--- other --
-Preposition: word placed before noun/pronoun to
- form phrase without modifying another word in sentance
-
-Conjunction: joins words, phrases, clauses,
-
-Interjection: Word used to express emotion
-    (most likely we do not need to use this type of word)
+    . period : Indicates a declaration or imperative
+        -- (Since we are not evaluating Interjections, )
+    ? query  : Denotes a question
+        -- In our case this could mean a goal we are trying to prove
+        -- or the head of a subproof we want to prove
+    ! exclaim: Denotes an exclamation or (for our program) imperative
+    """
+    PERIOD="."
+    QUESTION="?"
+    EXCLAIM="!"
+    COMMA=","
+    SEMICOLON=";"
+    COLON=":"
 
 
-'''
+
+class PartsOfSpeech:
+    """
+    8 Parts of Speech
+
+    -- Main words --
+    Verb : action or state of being
+    Noun: person, place, or thing
+    Pronoun: Word used in place of a noun
+
+    -- modifiers/descriptors--
+    Adjective: modifies or describes a noun or pronoun
+    Adverb: modifies or describes a verb
+
+    -- other --
+    Preposition: word placed before noun/pronoun to
+    form phrase without modifying another word in sentance
+
+    Conjunction: joins words, phrases, clauses,
+
+    Interjection: Word used to express emotion
+        (most likely we do not need to use this type of word)
+
+
+    """
+    NOUN=0
+    VERB=1
+    PRONOUN=2
+    ADJECTIVE=3
+    ADVERB=4
+    PREPOSITION=5
+    CONJUNCTION=6
+    INTERJECTION=7
 
 class Word(string):
     """
@@ -69,21 +93,10 @@ class Verb(Word):
     """
     Represents a Verb in the English Language
     """
+    pass
 
-class Punctuation:
-    period="."
-    question="?"
-    exclaim="!"
-    comma=","
-    semicolon=";"
-    colon=":"
-    
 
-class SentanceTypes:
-    declaration=0
-    imperative=1
-    interrogative=2
-    exclamation=3
+
 
 class Sentance(object):
     """
@@ -123,30 +136,50 @@ class AnalyzedSentance(Sentance):
     """
 
     def __init__(self,rawtext="",dictionary="",grammarbook=""):
+        '''Construct an AnalyzedSentance object'''
         Sentance.__init__(self,rawtext)
         self.words = __determineClassifiedWords()
-        self.sentanceType = 
-    '''
-    Makes analysis of sentance to find commas and conjunctions which do not represent 
-    collections gathering in order to find parts of this sentance which themselves
-    could be considered sentances
-    ex. "I am a witch, but are you a witch?"
-        I am a witch. (declaration) are you a witch? (question)
-    '''
+        self.sentanceType = SentanceTypes.declaration
+    
     def __determineIndependentClauses(self):
+        """
+        Makes analysis of sentance to find commas and conjunctions which do not represent 
+        collections gathering in order to find parts of this sentance which themselves
+        could be considered sentances
+        ex. "I am a witch, but are you a witch?"
+            I am a witch. (declaration) are you a witch? (question)
+        """
         pass
-    '''
-    Returns a list of tuples (word,index of occurence) where each word is classified
-    into parts of speech (word will be one of the childclasses of "Word")
-    '''
+    
     def __determineClassifiedWords(self):
-        pass
-
-    '''
-    Returns the type of sentance. Will either be DECLARATION
-    '''
-    def __determineSentanceType(self):
+        """
+        Returns a list of tuples (word,index of occurence) where each word is classified
+        into parts of speech (word will be one of the childclasses of "Word")
+        """
         pass
 
     
+    def __determineSentanceType(self):
+        """
+        Returns the type of sentance. Will either be DECLARATION
+        """
+        pass
+
+    def __classifyWord(self,word):
+        """
+        Will classify a word using the dictionary specified in the constructor
+        """
+        pass
+
+ #
+ #  MODULE METHODS
+ #    
+
+def loadWordDictionary(filePath):
+    """
+    Returns a dictionary which establishes words to parts of speech
+    """ 
+    pass
+
+  
 
